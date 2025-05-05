219d2b6b1cf707a3f69a3ddc19be26984e5a7943
alexis magnier <105195347+Rom1Stick@users.noreply.github.com>
2025-05-05 11:02:44 +0100

    fix(Ci/CD): uni test debug

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,10 +1,13 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: {},
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
   test: {
     globals: true,
