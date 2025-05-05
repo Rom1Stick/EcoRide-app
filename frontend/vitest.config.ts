273e093886feab0f844e7c0ac1536c98d722b466
alexis magnier <105195347+Rom1Stick@users.noreply.github.com>
2025-05-05 10:04:04 +0100

    fix(Ci/CD): vite plugin update

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -4,9 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: {
-      '@jest/globals': 'vitest',
-    },
+    alias: {},
   },
   test: {
     globals: true,
